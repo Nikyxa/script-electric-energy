cdef227600c188509fce329f6cefb8aa473643a2
Nikyxa <kolisova.veronika81@gmail.com>
2023-06-13 15:58:09 +0300

    Add flask app and edit main.py

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="mysql",
+    password="mysql123",
+    database="electricity_market_data",
+)
+
+
+@app.route("/data", methods=["GET"])
+def get_data():
+    if not date:
+        return jsonify({"error": "No date provided"})
+
+    cursor = db.cursor()
+
+    query = """SELECT * FROM electricity_market_data WHERE date = %s"""
+    cursor.execute(query, (date,))
+    result = cursor.fetchall()
+
+    cursor.close()
+    db.close()
+
+    data = []
+    for row in result:
+        record = {
+            "date": row[0],
+            "hour": row[1],
+            "price": row[2],
+            "volume": row[3],
+        }
+        data.append(record)
+
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run()
--- main.py
+++ main.py
@@ -5,10 +5,8 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import mysql.connector
 
-# Set up the Chrome driver
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-# Set up the MySQL connection
 db = mysql.connector.connect(
     host="localhost",
     user="mysql",
@@ -17,20 +15,16 @@ db = mysql.connector.connect(
 )
 cursor = db.cursor()
 
-# Navigate to the URL
 url = "https://www.oree.com.ua/index.php/control/results_mo/DAM"
 driver.get(url)
 
-# Click on the button
 button = driver.find_element(By.XPATH, "//*[@id='trade_res_type']/div[3]")
 button.click()
 
-# Get the items
 items = driver.find_elements(
     By.XPATH, "/html/body/div[4]/div[1]/div[3]/div[3]/div/div[2]/table/tbody/tr"
 )
 
-# Iterate over the items
 for item in items:
     item_text = item.text
     data = item_text.split(" ")
@@ -53,7 +47,6 @@ for item in items:
 
     db.commit()
 
-# Close the driver and database connection
 driver.quit()
 cursor.close()
 db.close()
